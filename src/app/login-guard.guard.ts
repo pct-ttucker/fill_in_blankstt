37c92d70f630377e6eb056273cbbeee90f27a8e6
pct-cclausen <51796051+pct-cclausen@users.noreply.github.com>
2022-09-20 11:01:44 +0200

    1 file modified

--- src/app/login-guard.guard.ts
+++ src/app/login-guard.guard.ts
@@ -7,6 +7,11 @@ import {
 } from '@angular/router';
 import { Observable } from 'rxjs';
 
+/**
+ * This is a Guard which only allows to navigate through it iff there is a current-user set in the local storage.
+ * "Login" in our app just means that a user name was provided for which points are collected,
+ * thus this Guard is all it needs to prevent users from opening pages which require the current-user.
+ */
 @Injectable({ providedIn: 'root' })
 export class LoginGuard implements CanActivate {
   constructor(public router: Router) {}
